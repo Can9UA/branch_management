1abf28e4072ae4fa5255dd3a15e9ccccf069a6ea
alexandr poddubnyi <alexandr.poddubnyi@ssa-outsourcing.com>
2017-06-08 14:36:41 +0300

    update code styles

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,27 +4,29 @@ import { BranchesComponent } from './branches/branches.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{
-  path: 'branches',
-  component: BranchesComponent,
-  data: {
-    breadcrumb: 'Branches',
-    title: 'Branches' // page title
+const routes: Routes = [
+  {
+    path: 'branches',
+    component: BranchesComponent,
+    data: {
+      breadcrumb: 'Branches',
+      title: 'Branches' // page title
+    }
+  }, {
+    path: 'employees',
+    component: EmployeesComponent,
+    data: {
+      breadcrumb: 'Employees',
+      title: 'Employees'
+    }
+  }, {
+    path: '',
+    component: HomeComponent,
+    data: {
+      breadcrumb: 'Home',
+      title: 'Home'
+    }
   }
-}, {
-  path: 'employees',
-  component: EmployeesComponent,
-  data: {
-    breadcrumb: 'Employees',
-    title: 'Employees'
-  }
-}, {
-  path: '',
-  component: HomeComponent,
-  data: {
-    breadcrumb: 'Home',
-    title: 'Home'
-  }
-}];
+];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(routes);
